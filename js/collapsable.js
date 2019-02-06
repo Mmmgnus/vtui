879604aef1d5e71fd4e736514ac0265b5fdf5e65
Magnus Fredlundh <magnusfredlundh@gmail.com>
2019-02-06 23:03:11 +0100

    Init

---
+++ js/collapsable.js
@@ -0,0 +1,14 @@
+export  function initCollapsable (context) {
+	const container = context || document.body;
+
+	container.addEventListener('click', function (event) {
+		const test_item = event.target.closest('[data-collapsable-target]');
+
+		if (!test_item) {
+			return;
+		}
+
+		document.getElementById(test_item.dataset.collapsableTarget).classList.toggle('c-collapsable--collapsed');
+	});
+
+}
